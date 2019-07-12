9d7707bcde0df9ab1cc48448fd4137829a0e9182
Patrick Frémont <47543532+PaFremont@users.noreply.github.com>
2019-07-12 17:23:52 +0200

    Update: NCS Sample database and partition.py README.md notebbooks notebooks

--- notebooks/MyNoteBook/Libs.py
+++ notebooks/MyNoteBook/Libs.py
@@ -1,3 +1,32 @@
 # Databricks notebook source
 # MAGIC %sh 
 # MAGIC pip install --upgrade pip
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC pip list
+
+# COMMAND ----------
+
+dbutils.library.help()
+
+# COMMAND ----------
+
+import pkg_resources
+pkg_resources.get_distribution('scipy').version
+
+
+# COMMAND ----------
+
+dbutils.library.installPyPI('scipy','1.2.0')
+dbutils.library.restartPython()
+dbutils.library.list()
+
+# COMMAND ----------
+
+import pkg_resources
+pkg_resources.get_distribution('scipy').version
+
+# COMMAND ----------
+
